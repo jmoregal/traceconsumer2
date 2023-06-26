d84aca46a4dcd2dcbd4156ebdc8ba1e7cff56b26
jmoregal <juan.moreno.galiano@alumnos.upm.es>
2023-06-26 16:11:33 +0200

    Commit version 1.1

---
+++ src/main/java/es/upm/dit/apsv/traceconsumer2/Repository/TraceRepository.java
@@ -0,0 +1,7 @@
+package es.upm.dit.apsv.traceconsumer2.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+import es.upm.dit.apsv.traceconsumer2.model.Trace;
+
+public interface TraceRepository extends CrudRepository<Trace,String> {
+}
