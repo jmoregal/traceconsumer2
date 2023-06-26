5d1cd4ede4453e62b8b2449e89b17e6fe88ae8f0
jmoregal <juan.moreno.galiano@alumnos.upm.es>
2023-06-26 17:39:08 +0200

    Ahora mejor?

--- src/main/java/es/upm/dit/apsv/traceconsumer2/Repository/TraceRepository.java
+++ src/main/java/es/upm/dit/apsv/traceconsumer2/Repository/TraceRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package es.upm.dit.apsv.traceconsumer2.Repository;
 
 import org.springframework.data.repository.CrudRepository;
@@ -6,12 +5,3 @@ import es.upm.dit.apsv.traceconsumer2.model.Trace;
 
 public interface TraceRepository extends CrudRepository<Trace,String> {
 }
-=======
-package es.upm.dit.apsv.traceconsumer2.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import es.upm.dit.apsv.traceconsumer2.model.Trace;
-
-public interface TraceRepository extends CrudRepository<Trace,String> {
-}
->>>>>>> 711a93f65287f759948c2125d9b4f84836a48e6f
--- src/main/resources/loadorders.sh
+++ src/main/resources/loadorders.sh
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 while IFS= read -r line; do
 curl -H "Content-Type: application/json" -X PUT --data-binary "$line" http://localhost:8080/orders;
 done < orders.json
-=======
-while IFS= read -r line; do
-curl -H "Content-Type: application/json" -X PUT --data-binary "$line" http://localhost:8080/orders;
-done < orders.json
->>>>>>> 711a93f65287f759948c2125d9b4f84836a48e6f
